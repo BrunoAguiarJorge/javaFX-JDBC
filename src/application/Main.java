6d3ad8ca49a5dde14658748b0ac64b34bfffec82
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-01-03 21:43:23 +0000

    AboutView

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,8 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		//Create an object Scene and load MainView in Main.
@@ -20,7 +22,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -28,4 +30,8 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,50 +1,76 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
-public class MainViewController implements Initializable{
+public class MainViewController implements Initializable {
 
-	
-	//menu attributes 
+	// menu attributes
 	@FXML
 	private MenuItem menuItemSeller;
-	
+
 	@FXML
 	private MenuItem menuItemDepartment;
 
 	@FXML
 	private MenuItem menuItemAbout;
 
-	
-	//Create eventHandlers for the menu attributes
+	// Create eventHandlers for the menu attributes
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
-		
+
 	}
-	
+
 	@FXML
 	public void onmenuItemDepartmentAction() {
 		System.out.println("onmenuItemDepartment");
-		
+
 	}
-	
+
 	@FXML
 	public void onmenuItemAboutAction() {
-		System.out.println("onmenuItemAbout");
-		
+		loadView("/gui/About.fxml");
+
 	}
-	
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		// TODO Auto-generated method stub
-		
-	}
 
-	
+	}
+	private synchronized void loadView(String absoluteName) {
+
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+
+			// get main Scene
+			Scene mainScene = Main.getMainScene();
+			// Access scrollpane from mainView and get the root and the content in it.
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			// get the main menu + the windows in open ex: About
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 }
